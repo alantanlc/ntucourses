375327b19031a87494da9930f5034672a1b21b4b
Alan Tan <alanwuha91@gmail.com>
2020-04-07 19:10:25 +0800

    added new model 'class' and fixed bugs in scrape.py

--- backend/backend/scraping/management/commands/scrape.py
+++ backend/backend/scraping/management/commands/scrape.py
@@ -10,24 +10,29 @@ from django.utils import timezone
 
 from scraping.models import Course
 
+import sys
+
 class Command(BaseCommand):
     help = "collect courses"
 
-    # List of keywords
-    keywords = {}
-    keywords['prereq'] = 'Prerequisite:'
-    keywords['mutual'] = 'Mutually exclusive with:'
-    keywords['na_prog'] = 'Not available to Programme:'
-    keywords['na_all_prog'] = 'Not available to all Programme with:'
-    keywords['na_core'] = 'Not available as Core to Programme:'
-    keywords['na_pe'] = 'Not available as PE to Programme:'
-    keywords['na_ue'] = 'Not available as UE to Programme:'
-    keywords['grade'] = 'Grade Type:'
+    def get_prerequisite_text(self, course_texts):
+        prereq_texts = []
+        if 'Prerequisite:' in course_texts:
+            prereq_index = course_texts.index('Prerequisite:')
+            for text in course_texts[prereq_index+1:-1]:
+                if text == 'Prerequisite:':
+                    pass
+                elif ':' in text and 'Min Grade' not in text:
+                    break
+                elif text:
+                    prereq_texts.append(text)
+        result = ' '.join(prereq_texts)
+        return result
 
     # Define logic of command
     def handle(self, *args, **options):
         # Construct URL request information
-        url = "https://wish.wis.ntu.edu.sg/webexe/owa/AUS_SUBJ_CONT.main_display1"
+        url = 'https://wish.wis.ntu.edu.sg/webexe/owa/AUS_SUBJ_CONT.main_display1'
         user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'
         values = {
             'acadsem': '',
@@ -59,7 +64,7 @@ class Command(BaseCommand):
 
                 # Get all relevant course tags
                 courses = soup.select("td[width],font[color='#FF00FF'],font[color='BROWN'],font[color='GREEN'],font[color='RED']")[4:]
-                course_start_indices = [0] + [i+1 for i, course in enumerate(courses[:-1]) if '650' in course.attrs.values()]
+                course_start_indices = [i-1 for i, course in enumerate(courses) if '400' in course.attrs.values()]
 
                 # Get number of courses
                 num_of_courses = len(course_start_indices)
@@ -83,7 +88,7 @@ class Command(BaseCommand):
                         title = course_texts[1]
                         description = course_texts[-1]
                         academic_units = course_texts[2]
-                        prereq = course_texts[course_texts.index('Prerequisite:')+1] if 'Prerequisite:' in course_texts else ''
+                        prereq = self.get_prerequisite_text(course_texts)                        
                         mutual = course_texts[course_texts.index('Mutually exclusive with:')+1] if 'Mutually exclusive with:' in course_texts else ''
                         na_prog = course_texts[course_texts.index('Not available to Programme:')+1] if 'Not available to Programme:' in course_texts else ''
                         na_all_prog = course_texts[course_texts.index('Not available to all Programme with:')+1] if 'Not available to all Programme with:' in course_texts else ''
@@ -95,17 +100,19 @@ class Command(BaseCommand):
                         # # Save in db
                         Course.objects.update_or_create(
                             course_code=course_code,
-                            title=title,
-                            description=description,
-                            academic_units=academic_units,
-                            prerequisite=prereq,
-                            mutually_exclusive_with=mutual,
-                            not_available_to_programme=na_prog,
-                            not_available_to_all_programme_with=na_all_prog,
-                            not_available_as_core_to_programme=na_core,
-                            not_available_as_pe_to_programme=na_pe,
-                            not_available_as_ue_to_programme=na_ue,
-                            grade_type=grade_type,
+                            defaults= {
+                                'title': title,
+                                'description': description,
+                                'academic_units': academic_units,
+                                'prerequisite': prereq,
+                                'mutually_exclusive_with': mutual,
+                                'not_available_to_programme': na_prog,
+                                'not_available_to_all_programme_with': na_all_prog,
+                                'not_available_as_core_to_programme': na_core,
+                                'not_available_as_pe_to_programme': na_pe,
+                                'not_available_as_ue_to_programme': na_ue,
+                                'grade_type': grade_type,
+                            }
                         )
                         
                         course_statistics[sem]['saved'] += 1
--- backend/backend/scraping/models.py
+++ backend/backend/scraping/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class BaseModel(models.Model):
     created_datetime = models.DateTimeField(auto_now_add=True)
@@ -14,15 +15,7 @@ class Course(BaseModel):
     title = models.CharField(max_length=100)
     description = models.TextField()
     academic_units = models.FloatField()
-    
-    # prerequisite = models.CharField(max_length=1000, blank=True)
-    # mutually_exclusive_with = models.CharField(max_length=1000, blank=True)
-    # not_available_to_programme = models.CharField(max_length=1000, blank=True)
-    # not_available_to_all_programme_with = models.CharField(max_length=1000, blank=True)
-    # not_available_as_core_to_programme = models.CharField(max_length=1000, blank=True)
-    # not_available_as_pe_to_programme = models.CharField(max_length=1000, blank=True)
-    # not_available_as_ue_to_programme = models.CharField(max_length=1000, blank=True)
-
+    grade_type = models.BooleanField(default=False)
     prerequisite = models.TextField(blank=True)
     mutually_exclusive_with = models.TextField(blank=True)
     not_available_to_programme = models.TextField(blank=True)
@@ -30,14 +23,53 @@ class Course(BaseModel):
     not_available_as_core_to_programme = models.TextField(blank=True)
     not_available_as_pe_to_programme = models.TextField(blank=True)
     not_available_as_ue_to_programme = models.TextField(blank=True)
-    
-    grade_type = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.course_code
+        return f'{self.course_code}, {self.title}'
 
     class Meta:
         ordering = ['course_code']
 
     class Admin:
         pass
+
+# class Venue(BaseModel):
+#     facility = models.CharField(max_length=20, unique=True)
+#     location = models.CharField(max_length=20) # Formatted as BLOCK-LEVEL-UNIT, e.g. NS4-05-79
+#     lat = models.FloatField(blank=True)
+#     lng = models.FloatField(blank=True)
+    
+class Class(BaseModel):
+
+    CLASS_TYPE = (
+        ('lec', 'Lecture'),
+        ('tut', 'Tutorial'),
+        ('lab', 'Laboratory'),
+        ('sem', 'Seminar'),
+    )
+
+    DAY = (
+        ('mon', 'Monday'),
+        ('tue', 'Tuesday'),
+        ('wed', 'Wednesday'),
+        ('thu', 'Thursday'),
+        ('fri', 'Friday'),
+        ('sat', 'Saturday'),
+        ('sun', 'Sunday'),
+    )
+
+    class_type = models.CharField(max_length=3, choices=CLASS_TYPE, blank=True)
+    group = models.CharField(max_length=6, blank=True)
+    day = models.CharField(max_length=3, choices=DAY, blank=True)
+    start_time = models.TimeField(null=True, blank=True)
+    end_time = models.TimeField(null=True, blank=True)
+    # venue = models.ForeignKey(Venue, on_delete=models.SET_NULL, null=True)
+    venue = models.CharField(max_length=20, blank=True)
+    remark = models.CharField(max_length=50, blank=True)
+
+    course_code = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True)
+    year = models.PositiveIntegerField(validators=[MinValueValidator(2019), MaxValueValidator(9999)])
+    semester = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(4)])
+
+    class Meta:
+        ordering = ['course_code', '-year', '-semester']
