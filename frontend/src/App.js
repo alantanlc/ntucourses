7cef1c6ce97c23c3396ddb0204bfb54b9aef4f77
Alan Tan <alanwuha91@gmail.com>
2020-04-14 21:03:17 +0800

    hello world react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Hello World!
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
