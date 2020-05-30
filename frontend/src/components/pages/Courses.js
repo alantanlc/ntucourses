3f70c02c8b0dae919699c01ebfdc035873b0c027
Alan Tan <alanwuha91@gmail.com>
2020-05-30 14:40:35 +0800

    updated api url

--- frontend/src/components/pages/Courses.js
+++ frontend/src/components/pages/Courses.js
@@ -203,11 +203,11 @@ export class Courses extends Component {
         this.setState({
             is_loading: true
         })
-        return axios.get(`https://ntu-courses.df.r.appspot.com/courses/${this.props.location.search}`)
+        return axios.get(`https://ntucoursesprod.df.r.appspot.com/courses/${this.props.location.search}`)
     }
 
     getProgrammes() {
-        return axios.get(`https://ntu-courses.df.r.appspot.com/programmes/`)
+        return axios.get(`https://ntucoursesprod.df.r.appspot.com/programmes/`)
     }
 
     search = (keyword) => {
