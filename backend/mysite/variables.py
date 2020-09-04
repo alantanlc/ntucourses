e41d14b02c957491f647e6002c1b2015415beab7
Alan Tan <alanwuha91@gmail.com>
2020-09-04 23:36:59 +0800

    Change variable names

--- backend/mysite/variables.py
+++ backend/mysite/variables.py
@@ -1,8 +1,11 @@
+from dotenv import load_dotenv
+load_dotenv(verbose=True)
+
 import os
 
-print(f"SECRET_KEY={os.getenv('NTUCOURSES_DJANGO_SECRET_KEY', None)}")
-print(f"ENVIRONMENT={os.getenv('NTUCOURSES_ENVIRONMENT', None)}")
-print(f"HOST={os.getenv('NTUCOURSES_HOST', None)}")
-print(f"PORT={os.getenv('NTUCOURSES_PORT', None)}")
-print(f"NAME={os.getenv('NTUCOURSES_NAME', None)}")
-print(f"PASSWORD={os.getenv('NTUCOURSES_PASSWORD', None)}")
+print(f"SECRET_KEY={os.getenv('SECRET_KEY', None)}")
+print(f"ENVIRONMENT={os.getenv('ENVIRONMENT', None)}")
+print(f"HOST={os.getenv('HOST', None)}")
+print(f"PORT={os.getenv('PORT', None)}")
+print(f"NAME={os.getenv('NAME', None)}")
+print(f"PASSWORD={os.getenv('PASSWORD', None)}")
