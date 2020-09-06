31e435aeb0890d9de57dece53aee5cb71c0cd218
Alan Tan <alanwuha91@gmail.com>
2020-09-07 00:23:49 +0800

    Disable admin page

--- backend/mysite/urls.py
+++ backend/mysite/urls.py
@@ -19,7 +19,7 @@ from django.urls import include, path
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('', include('api.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
