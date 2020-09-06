607e9de97c173660a6bddc98d433d61d11d4d9ba
Alan Tan <alanwuha91@gmail.com>
2020-09-07 00:28:37 +0800

    Disable auth page

--- backend/mysite/urls.py
+++ backend/mysite/urls.py
@@ -21,5 +21,5 @@ from django.urls import include, path
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', include('api.urls')),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
